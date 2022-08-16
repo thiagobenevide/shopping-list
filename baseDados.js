8ac6ddbc84eef1b76ff749edc9bab49d19897215
Thiago Benevide <thiagobenevide@live.com>
2022-08-16 16:02:34 -0300

    Codigo
    Ajustes da lista de compras

---
+++ baseDados.js
@@ -0,0 +1,30 @@
+class BaseDados{
+    var array = []
+
+
+    adicionarProduto(produto){
+        this.array.push(produto)
+    }
+
+    buscarProduto(id){
+        array.forEach(id => {
+            
+        });
+    }
+
+    isProduto(){
+
+    }
+
+    isProduto(){
+
+    }
+
+    removerProduto(){
+
+    }
+
+    atualizarProduto(){
+
+    }
+}
